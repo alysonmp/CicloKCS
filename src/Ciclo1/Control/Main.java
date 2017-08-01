41c0645843ba1f39a5f08a8a117aa37a917a215e
leoocastroo <leoocasto7@gmail.com>
2017-08-01 11:44:12 -0300

    Att - Conexao com o banco

--- src/Ciclo1/Control/Main.java
+++ src/Ciclo1/Control/Main.java
@@ -4,32 +4,13 @@
  * and open the template in the editor.
  */
 package Ciclo1.Control;
-
-import java.net.InetAddress;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.apache.derby.drda.NetworkServerControl;
-
 /**
  *
  * @author alysonmp
  */
 public class Main {
-   
-    private static String dbURL = "jdbc:derby://localhost:1527/3e;create=true;user=root;password=root";
-    
+       
     public static void main(String[] args){
-        try {
-            NetworkServerControl server = new NetworkServerControl(InetAddress.getByName("localhost"),1527);
-            server.start(null);
-            Class.forName("org.apache.derby.jdbc.ClientDriver");
-            Connection conn = DriverManager.getConnection(dbURL);
-        } catch (Exception ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
         ControlPrincipal principal = new ControlPrincipal();
     }
 }
--- src/Ciclo1/View/ViewEspera.java
+++ src/Ciclo1/View/ViewEspera.java
@@ -8,17 +8,9 @@ package Ciclo1.View;
 import Ciclo1.Control.ControlPrincipal;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 

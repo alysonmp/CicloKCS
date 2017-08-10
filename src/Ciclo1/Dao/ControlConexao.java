2cab711b41b568a3da4ced4f6ef56756e3079b16
Alyson Pereira <alyson.pereira@beeleads.com.br>
2017-08-10 10:54:42 -0300

    conexão consertada

--- src/Ciclo1/Dao/ControlConexao.java
+++ src/Ciclo1/Dao/ControlConexao.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import org.apache.derby.drda.NetworkServerControl;
 
 /**
@@ -19,7 +20,7 @@ import org.apache.derby.drda.NetworkServerControl;
  */
 public class ControlConexao {
     
-    private String dbURL = "jdbc:derby://localhost:1527/3e;create=true;user=root;password=root";
+    private String dbURL = "jdbc:derby://localhost:1528/3e_KCS;create=true;user=root;password=root";
     private Connection conn = null;
     
     public ControlConexao() {
@@ -29,7 +30,8 @@ public class ControlConexao {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
             conn = DriverManager.getConnection(dbURL);
         } catch (Exception ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Falha na conexão com o banco de dados, abra o programa novamente");
+            System.exit(0);
         }
     }
 
